ffeb1a9de99f3a3fa5b0df8bdb2688035ae9431f
Jose Valduz <jose.valduz@unet.edu.ve>
2026-02-25 17:17:54 -0400

    Agregar métodos llamar y cortar a la clase Celular

--- Clases y Objetos.py
+++ Clases y Objetos.py
@@ -2,12 +2,19 @@
 # snake_case
 # PascalCase para definir clases en python
 class Celular:
+  # Este es el metodo constructor necesario para crear atributos de instancia
   def __init__(self, marca, modelo, camara): #forma de referirse a si mismo
     self.marca = marca
     self.modelo = modelo
     self.camara = camara 
-     
+  
+  def llamar(self):
+    print(f'Estas Llamando desde un : {self.modelo}')   
+  
+  def cortar(self):
+    print(f'cortaste la llamada desde tu : {self.modelo}')   
   
 celular1 = Celular("Samsung","S23","48MP")
-
+celular1.llamar()
+celular1.cortar()
 print(celular1.marca)
